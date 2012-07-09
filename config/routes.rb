386029c2dca99224e1548d7161404203c69b3c65
Aldo Perez <aldoshin@gmail.com>
2012-07-09 10:41:13 -0500

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 EMenu::Application.routes.draw do
 
-  root :to => 'menu_items#index' 
-
   resources :menu_items
+
+  root to: 'static_pages#home' 
+
+  match '/about', to: "static_pages#about"
+  match '/contact', to: "static_pages#contact"
+  match '/help', to: "static_pages#help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
